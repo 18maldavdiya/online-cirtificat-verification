a0ae38a62a58436d24a1dfcf2e686a4a3fbda316
18maldavdiya <hanish2005b@gmail.com>
2026-06-25 18:12:14 +0530

    project

---
+++ Frontend/Login/Login.js
@@ -0,0 +1,80 @@
+// ==============================
+// Selecting HTML Elements
+// ==============================
+
+const form = document.getElementById("loginForm");
+
+const email = document.getElementById("email");
+
+const password = document.getElementById("password");
+
+
+// ==============================
+// Login Event
+// ==============================
+
+form.addEventListener("submit", function(event){
+
+    // Stop page from refreshing
+    event.preventDefault();
+
+
+    // Get user input
+
+    const userEmail = email.value.trim();
+
+    const userPassword = password.value.trim();
+
+
+    // Empty Validation
+
+    if(userEmail === "" || userPassword === ""){
+
+        alert("Please fill all fields.");
+
+        return;
+
+    }
+
+
+    // ==============================
+    // Dummy Login
+    // ==============================
+
+    if(userEmail === "admin@gmail.com"
+        &&
+        userPassword === "12345"){
+
+        alert("Welcome Admin");
+
+        window.location.href="../Admin/dashboard.html";
+
+    }
+
+    else if(userEmail === "college@gmail.com"
+        &&
+        userPassword === "12345"){
+
+        alert("Welcome Organization");
+
+        window.location.href="../Organization/dashboard.html";
+
+    }
+
+    else if(userEmail === "student@gmail.com"
+        &&
+        userPassword === "12345"){
+
+        alert("Welcome Student");
+
+        window.location.href="../Student/dashboard.html";
+
+    }
+
+    else{
+
+        alert("Invalid Email or Password");
+
+    }
+
+});
